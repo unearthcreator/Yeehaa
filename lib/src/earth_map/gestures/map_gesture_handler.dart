150c8c5517de26b6f9052049571cfc98314c1d09
unearthcreator <181272960+unearthcreator@users.noreply.github.com>
2025-01-13 05:47:43 +0100

    No iodea

--- lib/src/earth_map/gestures/map_gesture_handler.dart
+++ lib/src/earth_map/gestures/map_gesture_handler.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // for rootBundle
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
